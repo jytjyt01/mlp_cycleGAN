a726f6ecc46ecc7c901817607358e5f9d75357ff
yuting <s2303866@ed.ac.uk>
2023-03-14 14:10:06 +0000

    1002

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import argparse
 import itertools
 
@@ -20,7 +22,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batchSize', type=int, default=6, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='datasets/1001/', help='root directory of the dataset')
+parser.add_argument('--dataroot', type=str, default='datasets/horse2zebra/', help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100,
                     help='epoch to start linearly decaying the learning rate to 0')
@@ -216,9 +218,9 @@ for epoch in range(opt.epoch, opt.n_epochs):
 
     # Save models checkpoints
     if epoch % 20 == 19:
-        torch.save(netG_A2B.state_dict(), 'output/{}_netG_A2B.pth'.format(epoch))
-        torch.save(netG_B2A.state_dict(), 'output/{}_netG_B2A.pth'.format(epoch))
-        torch.save(netD_A.state_dict(), 'output/{}_netD_A.pth'.format(epoch))
-        torch.save(netD_B.state_dict(), 'output/{}_netD_B.pth'.format(epoch))
+        torch.save(netG_A2B.state_dict(), 'outputHW/{}_netG_A2B.pth'.format(epoch))
+        torch.save(netG_B2A.state_dict(), 'outputHW/{}_netG_B2A.pth'.format(epoch))
+        torch.save(netD_A.state_dict(), 'outputHW/{}_netD_A.pth'.format(epoch))
+        torch.save(netD_B.state_dict(), 'outputHW/{}_netD_B.pth'.format(epoch))
 
 ###################################
